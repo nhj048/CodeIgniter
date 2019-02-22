c4f319c005f10825e2ac69a4a83bab253e4bfaa0
nhj048 <nhj048@gmail.com>
2019-02-22 18:17:35 +0900

    Initial commit

---
+++ application/views/template/head.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="ko">
+<head>
+	<meta charset="utf-8">
+	<title></title>
+	<link rel="stylesheet" href="/www/CodeIgniter/public/css/jquery-ui.css">
+	<link rel="stylesheet" href="/www/CodeIgniter/public/css/prettydropdowns.css">
+	<link rel="stylesheet" href="/www/CodeIgniter/public/css/toggle-switch.css">
+	<link rel="stylesheet" href="/www/CodeIgniter/public/css/jquery-ui-slider-pips.css">
+	<link rel="stylesheet" href="/www/CodeIgniter/public/css/wan-spinner.css">
+	<link rel="stylesheet" href="/www/CodeIgniter/public/css/main.css">
+	<link rel="stylesheet" href="/www/CodeIgniter/public/css/list.css">
+	<link rel="stylesheet" href="/www/CodeIgniter/public/css/search.css">
+	<link rel="stylesheet" href="/www/CodeIgniter/public/css/ca_view2.css">
+
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+	<script src="/www/CodeIgniter/public/js/jquery-ui.js"></script>
+	<script src="/www/CodeIgniter/public/js/common.js"></script>
+	<script src="/www/CodeIgniter/public/js/main.js"></script>
+	<script src="/www/CodeIgniter/public/js/linksys.js"></script>
+	<script src="/www/CodeIgniter/public/js/search.js"></script>
+	<script src="/www/CodeIgniter/public/js/jquery.prettydropdowns.js"></script>
+	<script src="/www/CodeIgniter/public/js/jquery-ui-slider-pips.js"></script>
+	<script src="/www/CodeIgniter/public/js/wan-spinner.js"></script>
+	<script src="/www/CodeIgniter/public/js/ca_view.js"></script>
+</head>
+<body>
